cd626100c39d5c8fa0daf24fba5ea0a1c5e2b7de
Alex Komoroske <jkomoros@gmail.com>
2023-12-20 06:26:19 -0800

    Factor sprout config object.
    Part of #1.

--- src/sprout.ts
+++ src/sprout.ts
@@ -6,6 +6,7 @@ import {
 
 import {
     Path,
+    SproutConfig,
     SproutName,
     sproutConfigSchema
 } from './types.js';
@@ -15,6 +16,7 @@ const SPROUT_CONFIG_PATH = 'config.json';
 
 export class Sprout {
     _path : Path
+    _config?: SproutConfig
 
     constructor(path : Path) {
         this._path = path;
@@ -25,16 +27,24 @@ export class Sprout {
         return this._path;
     }
 
+    async config() : Promise<SproutConfig> {
+        if (!this._config) {
+            const sproutConfigPath = joinPath(this._path, SPROUT_CONFIG_PATH);
+            if (!await fileExists(sproutConfigPath)) {
+                throw new Error(`${this.name}: Config file ${sproutConfigPath} not found`);
+            }
+            const configData = await fileFetch(sproutConfigPath);
+            //Tnis will throw if invalid shape.
+            const config = sproutConfigSchema.parse(JSON.parse(configData));
+            if (!config) throw new Error(`${this.name}: No config`);
+        }
+        if (!this._config) throw new Error(`${this.name}: Couldn't create sprout`);
+        return this._config;
+    }
+
     //throws if invalid
     async validate() : Promise<void> {
-        const sproutConfigPath = joinPath(this._path, SPROUT_CONFIG_PATH);
-        if (!await fileExists(sproutConfigPath)) {
-            throw new Error(`${this.name}: Config file ${sproutConfigPath} not found`);
-        }
-        const configData = await fileFetch(sproutConfigPath);
-        //Tnis will throw if invalid shape.
-        const config = sproutConfigSchema.parse(JSON.parse(configData));
-        if (!config) throw new Error(`${this.name}: No config`);
-        //TODO: validate shape
+        //Will throw if invalid
+        await this.config();
     }
 }
