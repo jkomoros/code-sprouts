5f72e8d7126cd3f946ff4429a0e31155455f5dc1
Alex Komoroske <jkomoros@gmail.com>
2023-12-20 07:14:14 -0800

    Configure help.
    Part of #1.

--- src/index.ts
+++ src/index.ts
@@ -13,10 +13,14 @@ import {
 import {
     parse
 } from 'ts-command-line-args';
-import { Sprout } from './sprout.js';
+
+import {
+    Sprout
+} from './sprout.js';
 
 const cliOptions = z.object({
-    sprout: z.optional(pathSchema)
+    sprout: z.optional(pathSchema),
+    help: z.optional(z.boolean())
 });
 
 type CLIOptions = z.infer<typeof cliOptions>;
@@ -35,15 +39,25 @@ const main = async (opts : CLIOptions) : Promise<void> => {
 }
 
 (async() => {
-
-    //TODO: configure help
     const opts = parse<CLIOptions>({
         sprout: {
             type: String,
             optional: true,
             defaultOption: true,
             description: 'The sprout to run (path from the current working directory)'
+        },
+        help: {
+            type: Boolean,
+            optional: true,
+            alias: 'h',
+            description: 'Print this usage guide'
         }
+    }, {
+        headerContentSections: [{
+			header: 'code-sprout',
+			content: 'Runs sprouts'
+		}],
+		helpArg: 'help'
     });
 
 	await main(opts);
