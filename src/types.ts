ae0980a840d58d16ce7b1dfb9167230d53319d13
Alex Komoroske <jkomoros@gmail.com>
2023-12-20 06:40:52 -0800

    Switch SproutConfig.version to be a literal 0, not '0'.
    Part of #1.

--- src/types.ts
+++ src/types.ts
@@ -11,7 +11,7 @@ const sproutName = z.string();
 export type SproutName = z.infer<typeof sproutName>;
 
 export const sproutConfigSchema = z.object({
-    version: z.literal('0')
+    version: z.literal(0)
 });
 
 export type SproutConfig = z.infer<typeof sproutConfigSchema>;
