b4e7923a368a479468dda3149886e4cf3f6e66b8
Alex Komoroske <jkomoros@gmail.com>
2023-12-20 05:45:07 -0800

    A stub of a zod thing.
    Part of #1.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('Hello world');
+import {
+    z
+} from 'zod';
+
+const sproutConfigSchema = z.object({
+    version: z.literal('0')
+});
+
+export type SproutConfig = z.infer<typeof sproutConfigSchema>;
