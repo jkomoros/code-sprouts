21b8e4a25fa7066622a52f22ac86df694ee818a7
Alex Komoroske <jkomoros@gmail.com>
2023-12-20 07:19:30 -0800

    Rough in openai stuff, from prompt-garden.
    Part of #4.

---
+++ src/openai.ts
@@ -0,0 +1,26 @@
+import {
+	openai
+} from '@polymath-ai/ai';
+
+export const computePromptOpenAI = async (modelName : string, apiKey : string, prompt : string) : Promise<string> => {
+	const response = await fetch(openai(apiKey).chatCompletion({
+		model: modelName,
+		messages: [
+			{
+				role: 'user',
+				content: prompt
+			}
+		]
+		//TODO: allow passing other parameters
+	}));
+
+	//TODO: ideally we'd have stronger typing here 
+	const result = await response.json();
+	return result.choices[0].message?.content || '';
+};
+
+export const computeTokenCountOpenAI = async (_modelName : string,  text : string) : Promise<number> => {
+	const tokenizer = await import('gpt-tok');
+
+	return tokenizer.encode(text).length;
+};
