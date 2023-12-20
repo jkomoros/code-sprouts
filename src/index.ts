c35779724cf97567ba10bb394e242b5da535e6be
Alex Komoroske <jkomoros@gmail.com>
2023-12-20 06:19:48 -0800

    Move sproutConfig schema.
    Part of #1.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,1 @@
-import {
-    z
-} from 'zod';
-
-const sproutConfigSchema = z.object({
-    version: z.literal('0')
-});
-
-export type SproutConfig = z.infer<typeof sproutConfigSchema>;
+console.log('TODO: implement');
--- src/types.ts
+++ src/types.ts
@@ -5,3 +5,9 @@ import {
 const pathSchema = z.string();
 
 export type Path = z.infer<typeof pathSchema>;
+
+export const sproutConfigSchema = z.object({
+    version: z.literal('0')
+});
+
+export type SproutConfig = z.infer<typeof sproutConfigSchema>;
