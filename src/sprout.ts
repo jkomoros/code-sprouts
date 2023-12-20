333372021fa31bb79f02f1b49b092f433b6e2010
Alex Komoroske <jkomoros@gmail.com>
2023-12-20 06:33:12 -0800

    Stub in validating schema.
    Part of #1.

--- src/sprout.ts
+++ src/sprout.ts
@@ -14,11 +14,13 @@ import {
 //Relative to the sprout root
 const SPROUT_CONFIG_PATH = 'config.json';
 const SPROUT_INSTRUCTIONS_PATH = 'instructions.md';
+const SPROUT_SCHEMA_PATH = 'schema.ts';
 
 export class Sprout {
     _path : Path
     _config?: SproutConfig
     _baseInstructions? : string
+    _schemaText? : string
 
     constructor(path : Path) {
         this._path = path;
@@ -56,10 +58,23 @@ export class Sprout {
         return this._baseInstructions;
     }
 
+    async schemaText() : Promise<string> {
+        if (this._schemaText === undefined) {
+            const sproutSchemaPath = joinPath(this._path, SPROUT_SCHEMA_PATH);
+            if (!await fileExists(sproutSchemaPath)) {
+                throw new Error(`${this.name}: Schema file ${sproutSchemaPath} not found`);
+            }
+            this._schemaText = await fileFetch(sproutSchemaPath);
+        }
+        if (this._schemaText === undefined) throw new Error(`${this.name}: No schema`);
+        return this._schemaText;
+    }
+
     //throws if invalid
     async validate() : Promise<void> {
         //Will throw if invalid
         await this.config();
         await this.baseInstructions();
+        await this.schemaText();
     }
 }
