a62c7c42b1545a13d4d0bea12a8b92869c23e74e
Alex Komoroske <jkomoros@gmail.com>
2023-12-23 07:22:21 -0800

    Start rewroking completeAndParse JSON to be more intelligent.
    It still doesn't work, because it doesn't keep track of when a string is a key or a value in an object context, so it will leave a key but no value.
    
    Checking in to be able to switch to other computer.
    
    Part of #4.

--- src/util.ts
+++ src/util.ts
@@ -2,18 +2,59 @@ export const assertUnreachable = (x : never) : never => {
 	throw new Error('Exhaustiveness check failed: ' + String(x));
 };
 
+//TODO: test this
 export const completeAndParseJSON = (partialJSON : string) : unknown => {
-	try {
-		return JSON.parse(partialJSON);
-	} catch(error) {
-		switch(partialJSON.slice(-1, 1)) {
-		case '"':
-			partialJSON += '}';
+	if (!partialJSON) return null;
+	const thingsToTerminate : ('"' | ']' | '}')[] = [];
+	let previousCharWasEscape = false;
+	let inString = false;
+	
+	//TODO: this doesn't work. 
+	//the problem is that in an object context we need to know if the string we're in is a key or the value.
+	//A way to handle this is to look at the first thingsToTerminate, and if it's '}' then we need to know if we have a key but no value.
+
+	for (const char of partialJSON) {
+		let charIsEscape = false;
+		switch(char) {
+		case '\\':
+			charIsEscape = true;
 			break;
-		default:
-			partialJSON += '"}';
+		case '"':
+			if (previousCharWasEscape) break;
+			
+			if (inString) {
+				if (!thingsToTerminate.length || thingsToTerminate[0] != '"') throw new Error('String was not terminated by string');
+				thingsToTerminate.shift();
+			} else {
+				thingsToTerminate.unshift('"');
+			}
+			inString = !inString;
+			break;
+		case '[':
+			if (inString) break;
+			thingsToTerminate.unshift(']');
+			break;
+		case ']':
+			if (inString) break;
+			if (!thingsToTerminate.length || thingsToTerminate[0] != ']') throw new Error('Array not terminated');
+			thingsToTerminate.shift();
+			break;
+		case '{':
+			if (inString) break;
+			thingsToTerminate.unshift('}');
+			break;
+		case '}':
+			if (inString) break;
+			if (!thingsToTerminate.length || thingsToTerminate[0] != '}') throw new Error('Object not terminated');
+			thingsToTerminate.shift();
 			break;
 		}
-		return JSON.parse(partialJSON);
+		previousCharWasEscape = charIsEscape;
+	}
+	const finalString = partialJSON + thingsToTerminate.join('');
+	try {
+		return JSON.parse(finalString);
+	} catch(error) {
+		throw new Error(`Could not parse partial json *${finalString}*: ${error}`);
 	}
 };
