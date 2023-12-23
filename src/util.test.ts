3c984ac5b6a8871b26b210f8fe23b12023048e68
Alex Komoroske <jkomoros@gmail.com>
2023-12-23 08:48:53 -0800

    Fix eslint problem

--- src/util.test.ts
+++ src/util.test.ts
@@ -1,20 +1,20 @@
 import {
-    completeAndParseJSON
+	completeAndParseJSON
 } from './util';
 
 describe('test completeAndParseJSON', () => {
-  it('should not throw an error', () => {
-    const input = '';
-    expect(() => completeAndParseJSON(input)).not.toThrow();
-  });
+	it('should not throw an error', () => {
+		const input = '';
+		expect(() => completeAndParseJSON(input)).not.toThrow();
+	});
 
-  it('should not throw for starter object', () => {
-    const input = '{';
-    expect(() => completeAndParseJSON(input)).not.toThrow();
-  })
+	it('should not throw for starter object', () => {
+		const input = '{';
+		expect(() => completeAndParseJSON(input)).not.toThrow();
+	});
 
-  it('should not throw for starter object that starts a key', () => {
-    const input = '{"';
-    expect(() => completeAndParseJSON(input)).not.toThrow();
-  })
+	it('should not throw for starter object that starts a key', () => {
+		const input = '{"';
+		expect(() => completeAndParseJSON(input)).not.toThrow();
+	});
 });
