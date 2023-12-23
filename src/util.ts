c996a64fb3d1919af5c2859736052385615a46ec
Alex Komoroske <jkomoros@gmail.com>
2023-12-23 06:50:33 -0800

    Clean up indent problems.

--- src/util.ts
+++ src/util.ts
@@ -3,17 +3,17 @@ export const assertUnreachable = (x : never) : never => {
 };
 
 export const completeAndParseJSON = (partialJSON : string) : unknown => {
-    try {
-        return JSON.parse(partialJSON);
-    } catch(error) {
-        switch(partialJSON.slice(-1, 1)) {
-            case '"':
-                partialJSON += '}';
-                break;
-            default:
-                partialJSON += '"}';
-                break;
-        }
-        return JSON.parse(partialJSON);
-    }
+	try {
+		return JSON.parse(partialJSON);
+	} catch(error) {
+		switch(partialJSON.slice(-1, 1)) {
+		case '"':
+			partialJSON += '}';
+			break;
+		default:
+			partialJSON += '"}';
+			break;
+		}
+		return JSON.parse(partialJSON);
+	}
 };
