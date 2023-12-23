597e0cdc982e93b22f79cf6c68b8352a4c8a378e
Alex Komoroske <jkomoros@gmail.com>
2023-12-22 20:14:00 -0800

    Add a completeAndParse JSON.
    Will be useful for streaming. Part of #4.

--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,19 @@
 export const assertUnreachable = (x : never) : never => {
 	throw new Error('Exhaustiveness check failed: ' + String(x));
 };
+
+export const completeAndParseJSON = (partialJSON : string) : unknown => {
+    try {
+        return JSON.parse(partialJSON);
+    } catch(error) {
+        switch(partialJSON.slice(-1, 1)) {
+            case '"':
+                partialJSON += '}';
+                break;
+            default:
+                partialJSON += '"}';
+                break;
+        }
+        return JSON.parse(partialJSON);
+    }
+};
