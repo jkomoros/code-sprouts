4963e0e0322ba7997bce0889e12300e59cef4e95
Alex Komoroske <jkomoros@gmail.com>
2023-12-24 14:32:26 -0800

    Add another test that passes but I didn't think would.

--- src/streaming-json.test.ts
+++ src/streaming-json.test.ts
@@ -113,4 +113,21 @@ describe('test parseStreamingJSON', () => {
 		}
 	});
 
+	it('iteratively handles a long and complex bit of json number 3 including colon and comma', () => {
+		const input = `{
+			"userMessage": "This is a value: and another one,",
+			"patch": [
+				{
+					"op": "add",
+					"path": "/responses/-",
+					"value": "a"
+				}
+			]
+		}`;
+		for (let i = 0; i < input.length; i++) {
+			const str = input.slice(0,i);
+			expect(() => parseStreamingJSON(str)).not.toThrow();
+		}
+	});
+
 });
