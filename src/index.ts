465a02b993a7081ad675bb98d9a17a8778e50128
Alex Komoroske <jkomoros@gmail.com>
2023-12-24 12:10:36 -0800

    Switch to using default models in ai provider since they now owrk.

--- src/index.ts
+++ src/index.ts
@@ -121,7 +121,7 @@ const main = async (opts : CLIOptions) : Promise<void> => {
 
 	const ai = new AIProvider({
 		openai_api_key: OPENAI_API_KEY
-	}, 'openai.com:gpt-4-1106-preview');
+	});
 	const sprout = new Sprout(sproutName, ai);
 	await sprout.validate();
 	await runSprout(sprout, opts);
