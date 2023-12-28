ad497797f57a6b7daa4a0d7c2b1f610e1d41c3f9
Alex Komoroske <jkomoros@gmail.com>
2023-12-27 16:56:08 -0800

    Get app runner to actually work with a few twiddles.
    Part of #3.

--- app/runner.ts
+++ app/runner.ts
@@ -2,7 +2,7 @@ import {
 	Sprout
 } from '../src/sprout.js';
 
-//TODO: accept a way to signal that this should cancel.
+//TODO: accept a way to signal that this should stop (for example, right now if you select a different codename it doesn't end).
 export const runSproutInBrowser = async (sprout : Sprout) => {
 	await sprout.validate();
 
@@ -10,10 +10,13 @@ export const runSproutInBrowser = async (sprout : Sprout) => {
 	//TODO: support images
 	while(active) {
 		//TODO: stream to output
-		const message = await sprout.conversationTurn();
+		const message = await sprout.conversationTurn({
+			//For some reason if you don't pass a stream logger it doesn't work.
+			streamLogger: () => {}
+		});
 		console.log('result', message);
 		//TODO: better UI to allow user to provide a response.
-		const response = prompt('What is your response?');
+		const response = prompt(`The AI said:\n${message}\n\nWhat is your response?`);
 		sprout.provideUserResponse(response || '');
 	}
 };
