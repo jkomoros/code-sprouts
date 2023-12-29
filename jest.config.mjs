9d062ee81196330631107960753f19c257a648f2
Alex Komoroske <jkomoros@gmail.com>
2023-12-29 07:39:21 -0800

    Indentation

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,13 +1,13 @@
 export default {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  roots: ['<rootDir>/src'],
-  transform: {
-    '^.+\\.ts$': 'ts-jest',
-  },
-  testMatch: ['**/*.test.ts'],
-  //Ignore the extension of imports if provided so it doesn't conflict
-  moduleNameMapper: {
-    '^(\\.{1,2}/.*)\\.js$': '$1',
-  }
+	preset: 'ts-jest',
+	testEnvironment: 'node',
+	roots: ['<rootDir>/src'],
+	transform: {
+		'^.+\\.ts$': 'ts-jest',
+	},
+	testMatch: ['**/*.test.ts'],
+	//Ignore the extension of imports if provided so it doesn't conflict
+	moduleNameMapper: {
+		'^(\\.{1,2}/.*)\\.js$': '$1',
+	}
 };
