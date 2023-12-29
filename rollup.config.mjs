3dc079b69880d6f9821540fbf67fdc912deb8bd2
Alex Komoroske <jkomoros@gmail.com>
2023-12-29 05:52:27 -0800

    Create an asset-copying pipeline with `npm run build:copy`.
    The configuration is the same one rollup uses to copy over files.
    
    Part of #3.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -7,6 +7,20 @@ import commonjs from '@rollup/plugin-commonjs';
 import summary from 'rollup-plugin-summary';
 import dynamicImportVars from '@rollup/plugin-dynamic-import-vars';
 
+//Load up assets.json and parse it
+import {
+	readFileSync
+} from 'fs';
+
+const assets = JSON.parse(readFileSync('assets.json').toString());
+
+const finalAssets = assets.map((asset) => {
+	return {
+		src: asset,
+		dest: 'dist'
+	};
+});
+
 export default {
 	input: 'build/app/components/my-app.js',
 	output: {
@@ -17,14 +31,7 @@ export default {
 		minifyHTML(),
 		dynamicImportVars(),
 		copy({
-			targets: [
-				{ src: 'images', dest: 'dist' },
-				{ src: 'fonts', dest: 'dist' },
-				{ src: 'manifest.json', dest: 'dist' },
-				{ src: 'index.html', dest: 'dist' },
-				{ src: 'examples', dest: 'dist' },
-				{ src: 'sprouts', dest: 'dist' }
-			],
+			targets: finalAssets,
 		}),
 		resolve(),
 		terser({
