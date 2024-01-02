7e110013532f230560946c1f59a94d08f19a30e7
Alex Komoroske <jkomoros@gmail.com>
2024-01-02 09:46:07 -0500

    Create overlayFetcher, which wraps a fetcher in an overlay, where reads to the overlay path will read from the overlay.
    Part of #15.

---
+++ src/fetcher-overlay.ts
@@ -0,0 +1,91 @@
+import {
+	DirectoryInfo,
+	Fetcher,
+	FetcherWithoutListSprouts,
+	Path
+} from './types.js';
+
+import {
+	fileLastUpdatedFromDirectoryInfo,
+	listDirectoryFromDirectoryInfo,
+	readFileFromDirectoryInfo,
+	writeFileToDirectoryInfo
+} from './util.js';
+
+class OverlayFetcher {
+	private _fetcher : Fetcher;
+	private _pathPrefix : string;
+	private _directory : DirectoryInfo;
+
+	constructor(fetcher : Fetcher, pathPrefix : string, directory : DirectoryInfo) {
+		this._fetcher = fetcher;
+		this._pathPrefix = pathPrefix;
+		this._directory = directory;
+	}
+
+	get localWriteablePath() : Path {
+		return this._pathPrefix;
+	}
+	
+	set localWriteablePath(path : Path) {
+		throw new Error('OverlayFetcher doesn\'t support localWriteablePath');
+	}
+
+	private internalPath(path : Path) : Path {
+		const prefix = this._pathPrefix + '/';
+		if (!path.startsWith(prefix)) throw new Error(`Path ${path} doesn't start with ${prefix}`);
+		return path.slice(prefix.length);
+	}
+
+	pathIsLocalWriteable(path : Path) : boolean {
+		return path.startsWith(this._pathPrefix);
+	}
+
+	fileFetch(path : Path) : Promise<string> {
+		if (!this.pathIsLocalWriteable(path)) return this._fetcher.fileFetch(path);
+		const result = readFileFromDirectoryInfo(this._directory, this.internalPath(path));
+		return Promise.resolve(result);
+	}
+
+	fileExists(path : Path) : Promise<boolean> {
+		if (!this.pathIsLocalWriteable(path)) return this._fetcher.fileExists(path);
+		try {
+			readFileFromDirectoryInfo(this._directory, this.internalPath(path));
+			return Promise.resolve(true);
+		} catch (e) {
+			return Promise.resolve(false);
+		}
+	}
+
+	listDirectory(path : Path) : Promise<Path[]> {
+		if (!this.pathIsLocalWriteable(path)) return this._fetcher.listDirectory(path);
+		const paths = listDirectoryFromDirectoryInfo(this._directory, this.internalPath(path));
+		return Promise.resolve(paths);
+	}
+
+	mayWriteFile(path : Path) : boolean {
+		return this.pathIsLocalWriteable(path);
+	}
+
+	writeFile(path : Path, data : string) : Promise<void> {
+		if (!this.pathIsLocalWriteable(path)) return this._fetcher.writeFile(path, data);
+		writeFileToDirectoryInfo(this._directory, this.internalPath(path), data);
+		return Promise.resolve();
+	}
+
+	supportsLastUpdated() : boolean {
+		return true;
+	}
+
+	fileLastUpdated(path : Path) : Promise<Date | null> {
+		if (!this.pathIsLocalWriteable(path)) return this._fetcher.fileLastUpdated(path);
+		const result = fileLastUpdatedFromDirectoryInfo(this._directory, this.internalPath(path));
+		return Promise.resolve(result);
+	}
+
+
+}
+
+export const overlayFetcher = (fetcher : Fetcher, pathPrefix : string, directory : DirectoryInfo) : FetcherWithoutListSprouts => {
+	return new OverlayFetcher(fetcher, pathPrefix, directory);
+};
