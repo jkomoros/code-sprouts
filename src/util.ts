jkomoros/code-sprouts

commit 024eb8fd7cb5ad57356899a7c1f152f14154ea44

Make DirectoryInfo signature be the same as "NakedDirectoryInfo".
In the next commit we'll get rid of the second name.

Part of #15. Part of #7.

src/util.ts
@@ -12,7 +12,6 @@ import {
 	DirectoryInfo,
 	DirectoryListingFile,
 	Fetcher,
-	NakedDirectoryInfo,
 	sproutBaseNameSchema,
 	FileListingType
 } from './types.js';
@@ -98,33 +97,14 @@ export const trimExtraNewlines = (input : string) : string => {
 	return input.replace(/\n{3,}/g, '\n\n');
 };
 
-export const makeDirectoryInfo = (naked : NakedDirectoryInfo, timestamp : string) : DirectoryInfo => {
-	const result : DirectoryInfo = {
-		directories: {},
-		files: {}
-	};
-	for (const [name, value] of Object.entries(naked)) {
-		if (typeof value === 'object') {
-			result.directories[name] = makeDirectoryInfo(value, timestamp);
-			continue;
-		}
-		result.files[name] = value;
-	}
-	const directoryListing : DirectoryListingFile = {
-		directories: Object.keys(result.directories),
-		files: Object.keys(result.files)
-	};
-	result.files['directory.json'] = JSON.stringify(directoryListing, null, '\t');
-	return result;
-};
-
 export const writeDirectoryInfo = async (fetcher : Fetcher, info : DirectoryInfo, path : Path = '') : Promise<void> => {
-	for (const [directory, directoryInfo] of TypedObject.entries(info.directories)) {
-		await writeDirectoryInfo(fetcher, directoryInfo, joinPath(path, directory));
-	}
-	for (const [filename, content] of TypedObject.entries(info.files)) {
-		const filePath = joinPath(path, filename);
-		await fetcher.writeFile(filePath, content);
+	for (const [filename, content] of TypedObject.entries(info)) {
+		if (typeof content == 'string') {
+			const filePath = joinPath(path, filename);
+			await fetcher.writeFile(filePath, content);
+		} else {
+			await writeDirectoryInfo(fetcher, content, joinPath(path, filename));
+		}
 	}
 };
 
@@ -134,13 +114,17 @@ export const readFileFromDirectoryInfo = (info : DirectoryInfo, path : Path) : s
 	const firstPart = parts[0];
 	if (parts.length === 1) {
 		//We're at the end of the path.
-		if (firstPart in info.files) {
-			return info.files[firstPart];
+		if (firstPart in info) {
+			const result = info[firstPart];
+			if (typeof result != 'string') throw new Error(`${path} was a folder not a file`);
 		}
 		throw new Error(`File not found: ${path}`);
 	}
-	if (firstPart in info.directories) {
-		return readFileFromDirectoryInfo(info.directories[firstPart], parts.slice(1).join('/'));
+	if (firstPart in info) {
+		const directory = info[firstPart];
+		if (!directory) throw new Error(`Couldn't find ${directory}`);
+		if (typeof directory == 'string') throw new Error(`${directory} is a file not a directory`);
+		return readFileFromDirectoryInfo(directory, parts.slice(1).join('/'));
 	}
 	throw new Error(`File not found: ${path}`);
 };
@@ -151,21 +135,24 @@ export const listDirectoryFromDirectoryInfo = (info : DirectoryInfo, path : Path
 	const firstPart = parts[0];
 	if (parts.length === 1) {
 		//We're at the end of the path.
-		if (!(firstPart in info.directories)) return [];
-		const directory = info.directories[firstPart];
+		if (!(firstPart in info)) return [];
+		const directory = info[firstPart];
+		if (typeof directory == 'string') throw new Error(`${path} is a file not a directory`);
 		switch (type) {
 		case 'both':
-			return Object.keys(directory.directories).concat(Object.keys(directory.files));
+			return Object.keys(directory);
 		case 'directory':
-			return Object.keys(directory.directories);
+			return Object.keys(directory).filter(key => typeof directory[key] != 'string');
 		case 'file':
-			return Object.keys(directory.files);
+			return Object.keys(directory).filter(key => typeof directory[key] == 'string');
 		default:
 			assertUnreachable(type);
 		}
 	}
-	if (firstPart in info.directories) {
-		return listDirectoryFromDirectoryInfo(info.directories[firstPart], parts.slice(1).join('/'), type);
+	if (firstPart in info) {
+		const directory = info[firstPart];
+		if (typeof directory == 'string') throw new Error(`${path} is a file not a directory`);
+		return listDirectoryFromDirectoryInfo(directory, parts.slice(1).join('/'), type);
 	}
 	throw new Error(`File not found: ${path}`);
 };
@@ -184,17 +171,16 @@ export const writeFileToDirectoryInfo = (info : DirectoryInfo, path : Path, data
 	const firstPart = parts[0];
 	if (parts.length === 1) {
 		//We're at the end of the path.
-		info.files[firstPart] = data;
-		info.files[DIRECTORY_LISTING_FILE] = JSON.stringify(directoryListingForDirectoryInfo(info), null, '\t');
+		info[firstPart] = data;
+		info[DIRECTORY_LISTING_FILE] = JSON.stringify(directoryListingForDirectoryInfo(info), null, '\t');
 		return;
 	}
-	if (!(firstPart in info.directories)) {
-		info.directories[firstPart] = {
-			directories: {},
-			files: {}
-		};
+	if (!(firstPart in info)) {
+		info[firstPart] = {};
 	}
-	return writeFileToDirectoryInfo(info.directories[firstPart], parts.slice(1).join('/'), data);
+	const directory = info[firstPart];
+	if (!directory || typeof directory == 'string') throw new Error(`${path} is to a file`);
+	return writeFileToDirectoryInfo(directory, parts.slice(1).join('/'), data);
 };
 
 export const absoluteRegExp = (r : RegExp) : RegExp => {

src/sprout.ts
@@ -55,7 +55,6 @@ import fastJSONPatch from 'fast-json-patch';
 import {
 	assertUnreachable,
 	joinPath,
-	makeDirectoryInfo,
 	randomString,
 	trimExtraNewlines,
 	writeFileToDirectoryInfo,
@@ -794,14 +793,13 @@ const packagedSproutFromUncompiledNotNeedingAI = async (uncompiled : NakedUncomp
 
 //In this file because we need Sprout to be defined. and don't want a cycle from util.ts back t here
 const packagedSproutFromUncompiledImpl = async (uncompiled : NakedUncompiledPackagedSprout, ai? : AIProvider) : Promise<PackagedSprout> => {
-	const uncompiledPackedSprout = makeDirectoryInfo(uncompiled, new Date().toISOString());
 	const dummySproutName = 'example';
-	const sprout = new Sprout(dummySproutName, {ai, packagedSprout: uncompiledPackedSprout});
+	const sprout = new Sprout(dummySproutName, {ai, packagedSprout: uncompiled});
 	const compiled = await sprout.compiledData();
 	//TODO: can't I just verify the fetcher did something and now the file exists within it?
-	writeFileToDirectoryInfo(uncompiledPackedSprout, SPROUT_COMPILED_PATH, JSON.stringify(compiled, null, '\t'));
+	writeFileToDirectoryInfo(uncompiled, SPROUT_COMPILED_PATH, JSON.stringify(compiled, null, '\t'));
 	//This should be a reasonably safe assertion because we just wrote in the last file that needed to be there.
-	return uncompiledPackedSprout as PackagedSprout;
+	return uncompiled as PackagedSprout;
 };
 
 export const emptySprout = async () : Promise<PackagedSprout> => {

src/types.ts
@@ -226,36 +226,36 @@ export type Logger = (...messages : string[]) => void;
 export type StreamLogger = (input : string) => void;
 
 //NOTE: has to be kept in sync manually with DirectoryInfo.
-export const directoryInfoSchema: z.Schema<DirectoryInfo> = z.object({
-	directories: z.record(z.string(), z.lazy(() => directoryInfoSchema)),
-	files: z.record(z.string(), z.string())
-});
+export const directoryInfoSchema : z.Schema<DirectoryInfo> = z.record(
+	z.string(),
+	z.union([
+		z.string(),
+		z.lazy(() => directoryInfoSchema)
+	])
+);
 
 //NOTE: has to be kept in sync manually with directoryInfoSchema.
 export type DirectoryInfo = {
-    directories: Record<string, DirectoryInfo>,
-    files: Record<string, string>
+	[file : string]: string | DirectoryInfo
 }
 
+//TODO: get rid of NakedDiretoryInfo now that it's the same as DirectoryInfo.
+//TODO: get rid of NakedPackagedSprout
 export type NakedDirectoryInfo = {
 	[file : string]: string | NakedDirectoryInfo
 }
 
 //NOTE: needs to be kept up to date with file structure in constants.ts
 export const packagedSproutSchema = z.object({
-	directories: z.object({
-		sub_instructions: z.object({
-			directories: z.record(z.string(), z.never()),
-			files: z.record(subInstructionsFilenameSchema, z.string())
-		}).optional()
-	}),
-	files: z.object({
-		'directory.json': z.string(),
-		'sprout.json': z.string(),
-		'sprout.compiled.json': z.string(),
-		'instructions.md': z.string(),
-		'schema.ts': z.string().optional()
-	})
+	'directory.json': z.string(),
+	'sprout.json': z.string(),
+	'sprout.compiled.json': z.string(),
+	'instructions.md': z.string(),
+	'schema.ts': z.string().optional(),
+	sub_instructions: z.object({
+		directories: z.record(z.string(), z.never()),
+		files: z.record(subInstructionsFilenameSchema, z.string())
+	}).optional()
 });
 
 //NOTE: needs to be kept up to date with file structure in constants.ts

commit 9b0b8f1f15142c0dceaf5e5e289c19a992355043

Fix another bug caught by smoketest.
Broken since 024eb8fd7cb5ad57356899a7c1f152f14154ea44

Smoketest now passes.

Part of #15. Part of #7.

src/util.ts
@@ -160,8 +160,8 @@ export const listDirectoryFromDirectoryInfo = (info : DirectoryInfo, path : Path
 
 const directoryListingForDirectoryInfo = (info : DirectoryInfo) : DirectoryListingFile => {
 	return {
-		directories: Object.keys(info.directories),
-		files: Object.keys(info.files)
+		directories: Object.keys(info).filter(key => typeof info[key] != 'string'),
+		files: Object.keys(info).filter(key => typeof info[key] == 'string')
 	};
 };
 