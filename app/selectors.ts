56d0e7dd84145666f2a4666b0af3640e6c7b98b9
Alex Komoroske <jkomoros@gmail.com>
2024-01-05 16:20:43 -0800

    Create selectAPIKeys selector.
    Part of #5.

--- app/selectors.ts
+++ app/selectors.ts
@@ -18,6 +18,10 @@ import {
 	fetcher
 } from './fetcher.js';
 
+import {
+	ModelProvider
+} from '../src/types.js';
+
 export const selectPage = (state : RootState) => state.app ? state.app.page : '';
 export const selectPageExtra = (state : RootState) => state.app ? state.app.pageExtra : '';
 export const selectHash = (state : RootState) => state.app ? state.app.hash : '';
@@ -29,8 +33,7 @@ export const selectSproutStreaming = (state : RootState) => state.data ? state.d
 export const selectStreamCounter = (state : RootState) => state.data ? state.data.streamCounter : 0;
 export const selectDraftMessage = (state : RootState) => state.data ? state.data.draftMessage : '';
 export const selectAttachedImage = (state : RootState) => state.data ? state.data.attachedImage : null;
-export const selectOpenAIAPIKey = (state : RootState) => state.data ? state.data.apiKeys['openai.com'] : '';
-export const selectAnthropicAPIKey = (state : RootState) => state.data ? state.data.apiKeys['anthropic.com'] : '';
+export const selectAPIKeys = (state : RootState) : Record<ModelProvider, string> => state.data ? state.data.apiKeys : {'anthropic.com': '', 'openai.com': ''};
 export const selectEditorOpen = (state : RootState) => state.data ? state.data.editorOpen : false;
 export const selectIsEditing = (state : RootState) => state.data ? state.data.editing : false;
 export const selectChangesMade = (state : RootState) => state.data ? state.data.changesMade : false;
@@ -38,6 +41,16 @@ export const selectSproutSnapshot = (state : RootState) => state.data ? state.da
 export const selectWrittenSprouts = (state : RootState) => state.data ? state.data.writtenSprouts : {};
 
 export const selectHashForCurrentState = (_state : RootState) => '';
+
+export const selectOpenAIAPIKey = createSelector(
+	selectAPIKeys,
+	(keys) => keys['openai.com']
+);
+
+export const selectAnthropicAPIKey = createSelector(
+	selectAPIKeys,
+	(keys) => keys['anthropic.com']
+);
 
 //This will be a convenient place to extend later.
 export const selectDialogOpen = createSelector(
