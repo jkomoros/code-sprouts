8e009c8bedccb565eaabe8b0420b3c5782109bb2
Alex Komoroske <jkomoros@gmail.com>
2024-01-14 17:01:34 -0800

    Create a fetcher-directoryinfo.
    Part of #21.

---
+++ src/fetcher-directoryinfo.ts
@@ -0,0 +1,78 @@
+import {
+	DirectoryInfo,
+	Fetcher,
+	Path,
+	FileListingType
+} from './types.js';
+
+import {
+	listDirectoryFromDirectoryInfo,
+	readFileFromDirectoryInfo
+} from './util.js';
+
+export class DirectoryInfoFetcher {
+	private _pathPrefix : string;
+	private _directory : DirectoryInfo;
+	private _hasWrites : boolean = false;
+
+	constructor(pathPrefix : string, directory : DirectoryInfo) {
+		this._pathPrefix = pathPrefix;
+		this._directory = directory;
+	}
+
+	writeable(_path : Path) : boolean {
+		return true;
+	}
+
+	get localWriteablePath() : Path {
+		return this._pathPrefix;
+	}
+	
+	set localWriteablePath(path : Path) {
+		throw new Error('OverlayFetcher doesn\'t support localWriteablePath');
+	}
+
+	get overlayHasWrites() : boolean {
+		return this._hasWrites;
+	}
+
+	private internalPath(path : Path) : Path {
+		const prefix = this._pathPrefix + '/';
+		if (!path.startsWith(prefix)) throw new Error(`Path ${path} doesn't start with ${prefix}`);
+		return path.slice(prefix.length);
+	}
+
+	pathIsLocalWriteable(path : Path) : boolean {
+		return path.startsWith(this._pathPrefix);
+	}
+
+	fileFetch(path : Path) : Promise<string> {
+		if (!this.pathIsLocalWriteable(path)) throw new Error(`Path ${path} isn't local writeable`);
+		const result = readFileFromDirectoryInfo(this._directory, this.internalPath(path));
+		return Promise.resolve(result);
+	}
+
+	fileExists(path : Path) : Promise<boolean> {
+		if (!this.pathIsLocalWriteable(path)) throw new Error(`Path ${path} isn't local writeable`);
+		try {
+			readFileFromDirectoryInfo(this._directory, this.internalPath(path));
+			return Promise.resolve(true);
+		} catch (e) {
+			return Promise.resolve(false);
+		}
+	}
+
+	listDirectory(path : Path, type: FileListingType = 'both') : Promise<Path[]> {
+		if (!this.pathIsLocalWriteable(path)) throw new Error(`Path ${path} isn't local writeable`);
+		const paths = listDirectoryFromDirectoryInfo(this._directory, this.internalPath(path), type);
+		return Promise.resolve(paths);
+	}
+
+	async listSprouts() : Promise<Path[]> {
+		return [];
+	}
+}
+
+//Smoketest
+const _ : Fetcher = new DirectoryInfoFetcher('', {});
+
